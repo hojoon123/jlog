7fc4e0120da60616853b8a7a9a64932e272b3efa
hojoon123 <rhzn5512@naver.com>
2024-07-24 11:42:49 +0900

    [fix] config 로봇텍스트 관련 픽스

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-    siteUrl: 'https://dailyjlog.com', // 웹사이트의 기본 URL
-    generateRobotsTxt: true, // robots.txt 파일 생성 여부
-    sitemapSize: 7000, // 각 sitemap 파일의 최대 URL 수
-  };
+  siteUrl: 'https://dailyjlog.com',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+    ],
+    sitemap: 'https://dailyjlog.com/sitemap.xml',
+  },
+};
