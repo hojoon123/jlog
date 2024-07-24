fdce536fdf7eaec08358c0f260148c003b36fba4
hojoon123 <rhzn5512@naver.com>
2024-07-24 11:59:48 +0900

    [feat] 네이버 색인 추가

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,4 +1,6 @@
-import { redirect } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
+import getUserNameIntro from './posts/getUserNameIntro';
+
 
 interface PageProps {
   params: {
@@ -6,8 +8,19 @@ interface PageProps {
   };
 }
 
-export default function Page({ params }: PageProps) {
+export default async function Page({ params }: PageProps) {
   const { id: userId } = params;
-  redirect(`/${userId}/posts`);
-  return userId;
+
+  // 유저 존재 여부 확인
+  const userData = await getUserNameIntro(userId);
+
+  if (!userData) {
+    // 유저가 존재하지 않을 경우 404 페이지로 이동
+    notFound();
+  } else {
+    // 유저가 존재할 경우 리다이렉트
+    redirect(`/${userId}/posts`);
+  }
+
+  return null;
 }
