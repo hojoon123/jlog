a4e64c5504d4e31137800d241bcd05cd2288854f
hojoon123 <rhzn5512@naver.com>
2024-07-24 13:16:50 +0900

    [fix] 사파리 다크모드 css 수정

--- components/home/PeriodSelector.tsx
+++ components/home/PeriodSelector.tsx
@@ -18,7 +18,7 @@ const PeriodSelector = ({ currentPeriod }: { currentPeriod: string }) => {
 
   return (
     <div className="flex justify-end mb-4">
-      <select value={period} onChange={handleChange} className="border p-2 rounded bg-neutral-50 dark: bg-neutral-50">
+      <select value={period} onChange={handleChange} className="border p-2 rounded bg-white dark: bg-btn-background hover:bg-btn-background-hover">
         <option value="day">Day</option>
         <option value="week">Week</option>
         <option value="month">Month</option>
